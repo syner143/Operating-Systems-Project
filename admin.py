03d85c38289b66a16efe6df6f7eeb14543c65946
Mihai Daniel Stefanescu <96482856+syner143@users.noreply.github.com>
2022-05-05 02:13:31 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Dealer
+from .models import Employee
+from .models import Customer
+from .models import Medicine
+from .models import Purchase
+from .models import Appointemnt
+
+# Register your models here.
+
+admin.site.register(Dealer)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Medicine)
+admin.site.register(Purchase)
+admin.site.register(Appointemnt)
